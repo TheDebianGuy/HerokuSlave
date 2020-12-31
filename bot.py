e6937ed84ac569f71dcefa13cd95b24af8c39fdd
Debian Guy <64433908+TheDebianGuy@users.noreply.github.com>
2020-12-31 22:37:56 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,15 @@
 import discord, subprocess, os
 
-client = discord.Client()
+client = discord.Client() #declare discord client
 
 @client.event
-async def on_message(message):
-        if message.content.find("!exec")!=-1:
+async def on_message(message): #on new message
+        if message.content.find("!exec")!=-1: #and message.author.id == 1234
                 output = subprocess.getoutput(message.content.split("!exec ")[1])
                 if output=="":
-                        await message.channel.send("No output")
+                        await message.channel.send("[]") #cannot send an empty message
                 else:
-                        await message.channel.send(output)
-                print(message.content.split("!exec ")[1] + " | Executed by " + message.author.name)
+                        await message.channel.send("[" + output + "]")
+                print(message.content.split("!exec ")[1] + " | Executed by " + message.author.name) #at least log the commands since I haven't implemented any password protection
 
 client.run(os.getenv("TOKEN")) #don't forget to configure token env variable in heroku project settings, it is unsecure to have it in plaintext
