d0adf5e248ceeb085ab9e6de8fa48f9cd7dacbe7
Debian Guy <64433908+TheDebianGuy@users.noreply.github.com>
2021-04-01 21:53:55 +0200

    error handling and long message fix

--- bot.py
+++ bot.py
@@ -8,8 +8,12 @@ async def on_message(message): #on new message
                 output = subprocess.getoutput(message.content.split("!exec ")[1])
                 if output=="":
                         await message.channel.send("[]") #cannot send an empty message
+                elif len(output) > 1999: #2000 is max characters count on discord
+                        await message.channel.send("[Response is too long]")
                 else:
                         await message.channel.send("[" + output + "]")
                 print(message.content.split("!exec ")[1] + " | Executed by " + message.author.name) #at least log the commands since I haven't implemented any password protection
-
-client.run(os.getenv("TOKEN")) #don't forget to configure token env variable in heroku project settings, it is unsecure to have it in plaintext
+try:
+        client.run(os.getenv("TOKEN")) #don't forget to configure token env variable in heroku project settings, it is unsecure to have it in plaintext
+except Exception as e:
+        print(e)
